5bf54c8b4103cf4748f90a90e35456b07ee28bdc
neelprabhu <neelkprabhu@gmail.com>
2017-04-17 19:16:41 -0400

    New descriptions, outputs

--- intestine.m
+++ intestine.m
@@ -1,7 +1,12 @@
 % BME 260 Spring 2017
 % Modeling Blood Flow in Healthy and Anemic Physiology
 % Intestine: Consume nutrients, add water, ions, and glucose into blood.
+% INPUTS:
+%   b: Master struct with blood parameters out of heart.
+% OUTPUTS:
+%   bInt: Blood parameters out of intestine.
+%   fI  : Flow in/out.
 
-function bInt = intestine(b)
+function [bInt,fI] = intestine(b)
 
 end
--- lungs.m
+++ lungs.m
@@ -1,7 +1,12 @@
 % BME 260 Spring 2017
 % Modeling Blood Flow in Healthy and Anemic Physiology
 % Lung: Remove CO2 from blood, add O2 via hemoglobin. No consumption.
+% INPUTS:
+%   b: Master struct with blood parameters out of heart.
+% OUTPUTS:
+%   bLung: Blood parameters out of lung.
+%   fL   : Flow in/out.
 
-function bLung = lungs(b)
+function [bLung,fL] = lungs(b)
 
 end
