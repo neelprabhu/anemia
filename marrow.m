50016f47657ecb1539fc0f840fed32441fa154e7
mkwong27 <mkwong027@gmail.com>
2017-04-22 16:03:58 -0400

    Add files via upload

--- marrow.m
+++ marrow.m
@@ -7,6 +7,11 @@
 %   bBone: Blood parameters out of bone marrow.
 %   fB   : Flow in/out.
 
-function [bBone,fB] = marrow(b)
-
+function [bBone,fB,b.hemo] = marrow(b,i)
+if i<20
+    b.hemo=0.150*exp((20-20)*i/100)
+    else 
+    b.hemo=0.12+0.15*exp((20-28.1)*i/100)
+    end
+    
 end
