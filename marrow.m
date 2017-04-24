b0b0ebc1e9dd1fb8239cad7dd0ba3d2dcbd01828
neelprabhu <neelkprabhu@gmail.com>
2017-04-23 23:54:18 -0400

    Adding lots of stuff

--- marrow.m
+++ marrow.m
@@ -7,10 +7,11 @@
 %   bBone: Blood parameters out of bone marrow.
 %   fB   : Flow in/out.
 
-function [bBone, fB] = marrow(b,k)
+function [bBone, fB] = marrow(b)
+k = b.i; % Counter
 if k<20
     b.hemo = 0.150*exp((20-20)*k/100);
-    else 
+else
     b.hemo = 0.12+0.15*exp((20-28.1)*k/100);
 end
 bBone = b;
