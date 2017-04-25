a236391838d375aa83391e27483d39cbbc0fcc98
neelprabhu <neelkprabhu@gmail.com>
2017-04-25 19:28:29 -0400

    too much respiration

--- intestine.m
+++ intestine.m
@@ -9,4 +9,6 @@
 
 function [bInt,fI] = intestine(b)
 
+bInt = b;
+
 end
--- respir.m
+++ respir.m
@@ -8,12 +8,10 @@ MW_h2o = 18;
 ni_glu = b.glu/MW_glu; % moles/min
 ni_h20 = b.h2o/MW_h2o;
 ni_o2 = b.concO2/MW_o2;
-ni_co2 = b.co2/MW_co2; % NOT A THING YET
-
- 
+ni_co2 = b.concCO2/MW_co2;
 
 % determine limiting factor between glucose and oxygen
-R = min((ni_glu/1),(ni_o2/6)); 
+R = min((ni_glu/1),(ni_o2/6));
 
 nj_glu = ni_glu - 1*R;
 nj_h2o = ni_h20 + 6*R;
