530fba659e648f012dd66572ac71f15c3338b97d
neelprabhu <neelkprabhu@gmail.com>
2017-04-28 14:25:57 -0400

    no idea

--- marrow.m
+++ marrow.m
@@ -13,9 +13,20 @@ bBone = respir(b);
 k = bBone.i; % Counter
 % Modeling aplastic anemia.
 if k<20
+<<<<<<< Updated upstream
 
     bBone.hemo = 0.150*exp((20-20)*k/100);
 else
     bBone.hemo = 0.12+0.15*exp((20-28.1)*k/100);
 end
 bBone = b;
+=======
+<<<<<<< HEAD
+    b.hemo = 0.150*exp((20-20)*k/100); %hello this is Megan testing stuff and being a turtle
+=======
+    bBone.hemo = 0.150*exp((20-20)*k/100);
+>>>>>>> neel
+else
+    bBone.hemo = 0.12+0.15*exp((20-28.1)*k/100);
+end
+>>>>>>> Stashed changes
